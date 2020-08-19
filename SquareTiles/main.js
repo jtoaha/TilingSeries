810b7f62f22f1d9e9daff905b3d05558a8508349
Jamila Toaha <jtoahacoding@gmail.com>
2020-08-18 23:56:54 -0400

    got non-animated kaleidoscope colors for tiles

--- SquareTiles/main.js
+++ SquareTiles/main.js
@@ -2,25 +2,25 @@ let canvas = undefined
 let ctx = undefined
 var strokeWidth = 5
 var hw = 100
+let r, g, b
+r = Math.floor(Math.random() * 256)
+g = Math.floor(Math.random() * 256)
+b = Math.floor(Math.random() * 256)
+
 function start() {
   canvas = document.getElementById('square-canvas')
   ctx = canvas.getContext('2d')
-  mainLoop()
-}
-
-document.addEventListener('DOMContentLoaded', start)
-
-function update() {
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
   ctx.lineWidth = strokeWidth
-}
-
-function draw() {
   ctx.fillStyle = 'pink'
   ctx.fillRect(0, 0, canvas.width, canvas.height / 1.25)
 
   ctx.strokeRect(0, 0, hw, hw)
+
+
+
+
 
   let rowTracker = 1
   let iStart
@@ -29,9 +29,29 @@ function draw() {
     else iStart = (-3 / 4) * hw
     rowTracker++
     for (let i = iStart; i < canvas.width; i += (5 / 4) * hw) {
+      console.log(Math.floor(Math.random() * 256))
+      r = Math.floor(Math.random() * 256)
+      g = Math.floor(Math.random() * 256)
+      b = Math.floor(Math.random() * 256)
+
+      ctx.fillStyle = `rgba(${r},${g},${b}, .25)`
       ctx.strokeRect(i, j, hw, hw)
+
+      ctx.fillRect(i, j, hw, hw)
     }
   }
+  mainLoop()
+}
+
+document.addEventListener('DOMContentLoaded', start)
+
+function update() {
+
+
+}
+
+function draw() {
+
 }
 
 function mainLoop() {
