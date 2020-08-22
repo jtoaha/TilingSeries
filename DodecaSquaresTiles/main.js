35bd871c7b17a01cf29d6173f759ccb7bf63d64d
Jamila Toaha <jtoahacoding@gmail.com>
2020-08-22 02:33:17 -0400

    (feat) added adjustable num rings

--- DodecaSquaresTiles/main.js
+++ DodecaSquaresTiles/main.js
@@ -1,28 +1,47 @@
+//Canvas Properties
 var canvas = undefined
 var ctx = undefined
+let flag = ''
+
+//Default Settings for DodecaSquare Rings
 let numSquares = 12
 let degrees = (2 * Math.PI) / numSquares
+let theme = '#ffffff' // Light or Dark Mode
 
-let colorTheme = ['#ffcc00', '#ff9900', '#ff6600', '#bf0603', '#cc3399', '#990066', '#5a189a', '#3399cc', '#006699', '#ccee66', '#99cc33', '#669900']
+let colorTheme = [
+  '#ffcc00',
+  '#ff9900',
+  '#ff6600',
+  '#bf0603',
+  '#cc3399',
+  '#990066',
+  '#5a189a',
+  '#3399cc',
+  '#006699',
+  '#ccee66',
+  '#99cc33',
+  '#669900',
+]
 
+let colorAnimationCount
+let frameCount
 
-
-let theme = '#fffff' // for Light or Dark mode
-let colorAnimationCount = 0;
-let frameCount = 0;
-
-let numRings=5;
-let ringsAnimationCount = 0;
+let numRings
+let ringsAnimationCount
 
 function start() {
   canvas = document.getElementById('dodeca-rings')
   ctx = canvas.getContext('2d')
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
-  ctx.imageSmoothingEnabled = true;
-  ctx.imageSmoothingQuality = 'high';
+  ctx.imageSmoothingEnabled = true
+  ctx.imageSmoothingQuality = 'high'
   ctx.translate(canvas.width / 2, canvas.height / 2)
-  ctx.globalAlpha = 0.1;//.01 for color animation
+  ctx.globalAlpha = 0.1 //.01 for color animation
+  colorAnimationCount = 0
+  frameCount = 0
+  numRings = numRings || 6
+  ringsAnimationCount = 0
   mainLoop()
 }
 
@@ -31,8 +50,8 @@ document.addEventListener('DOMContentLoaded', start)
 function update() {}
 
 function draw() {
-  // ctx.fillStyle = 'olive'
-  // ctx.fillRect(0, 0, canvas.width, canvas.height)
+  //  ctx.fillStyle = 'black'
+  //  ctx.fillRect(-canvas.width/2, -canvas.height/2, canvas.width*2, canvas.height*2)
 
   //   degrees/2 = 30/2 = 15
   // SOH - sin15 = opposite/hypotenuse
@@ -42,37 +61,47 @@ function draw() {
   let circleRadius = 30
   let addHeight = 0
 
-  for(let i= 0; i <= ringsAnimationCount; i++) {
-    ctx.rotate(degrees/2)
-    addHeight+=createRing (circleRadius+addHeight)
+  for (let i = 0; i < ringsAnimationCount; i++) {
+    ctx.rotate(degrees / 2)
+    addHeight += createRing(circleRadius + addHeight)
   }
-
 
   ctx.restore()
 
-  if(frameCount++ === 10){
+  if (frameCount++ === 10) {
     colorAnimationCount++
     frameCount = 0
   }
-  if(frameCount++ ===30){
-    if(ringsAnimationCount < numRings) ringsAnimationCount++
-    frameCount = 0;
+  if (frameCount++ === 30) {
+    if (ringsAnimationCount < numRings) ringsAnimationCount++
+    frameCount = 0
   }
+  if (
+    colorAnimationCount >= colorTheme.length &&
+    ringsAnimationCount >= numRings
+  )
+    ctx.globalAlpha = 1
 }
 
 function mainLoop() {
-  update()
-  draw()
-  window.setTimeout(mainLoop, 1000 / 60)
+  if (flag === 'reset') {
+    flag = ''
+    start()
+  } else {
+    update()
+    draw()
+    window.setTimeout(mainLoop, 1000 / 60)
+  }
 }
 
 //Creates a ring from 12 squares
 // returns height
-function createRing(circleRadius){
+function createRing(circleRadius) {
   let squareSide = 2 * Math.sin(degrees / 2) * circleRadius
 
   let degreesUpdate
-  ctx.globalAlpha = 0.05;
+
+  ctx.globalAlpha = 0.03
   for (let i = 0; i < 12; i++) {
     ctx.rotate(degrees)
 
@@ -80,19 +109,38 @@ function createRing(circleRadius){
     ctx.translate(circleRadius, 0)
     ctx.rotate(-Math.PI / 4)
     //Adding conditional so that only certain colors can be animation
-    // if(i <= colorAnimationCount) {
+    if (i <= colorAnimationCount) {
       ctx.fillStyle = colorTheme[i]
       ctx.fillRect(0, 0, squareSide, squareSide)
-      ctx.strokeRect(0, 0, squareSide, squareSide)
-    //}
-
+      //ctx.strokeRect(0, 0, squareSide, squareSide)
+    }
 
     ctx.restore()
   }
 
   //To find radius of next corresponding ring need to find height that is added by the squares - return length of half a diagonal of the square
   // return squareSide*Math.sqrt(2)/2
-  return Math.sin(Math.PI/3)*squareSide
+  return Math.sin(Math.PI / 3) * squareSide
 }
 
+//Add Event Handlers to input items
+$(document).ready(function () {
+  $('input[type=range]').click(function () {
+    if (this.name === 'numRings') {
+      numRings = this.value
+      flag = 'reset'
+    }
 
+    if (this.name === 'theme') {
+      if (this.value === 'light') {
+        theme = 'white'
+        alpha = 0.25
+      }
+      if (this.value === 'dark') {
+        console.log(this.value)
+        theme = 'black'
+        alpha = 0.6
+      }
+    }
+  })
+})
