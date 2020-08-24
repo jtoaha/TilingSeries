9693ae2f8f68705abdf8743dca583b60ad6b9c72
Jamila Toaha <jtoahacoding@gmail.com>
2020-08-23 23:55:38 -0400

    (feat) added starter files for ArcTiles

---
+++ ArcTiles/main.js
@@ -0,0 +1,22 @@
+var canvas = undefined
+var canvasContext = undefined
+
+function start() {
+  canvas = document.getElementById('myCanvas')
+  canvasContext = canvas.getContext('2d')
+  mainLoop()
+}
+
+document.addEventListener('DOMContentLoaded', start)
+
+function update() {}
+
+function draw() {
+  canvasContext.fillStyle = 'blue'
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+}
+function mainLoop() {
+  update()
+  draw()
+  window.setTimeout(mainLoop, 1000 / 60)
+}
