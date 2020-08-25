61e7b9215e5b3d1213bad0ebcca5b74cc4fdd47c
Jamila Toaha <jtoahacoding@gmail.com>
2020-08-24 23:20:25 -0400

    (feat) added initial CircleTiles design

--- CircleTiles/main.js
+++ CircleTiles/main.js
@@ -1,8 +1,19 @@
 var canvas = undefined
 var canvasContext = undefined
 
-let numCircles = 12;
-let degrees = 2 * Math.PI / numCircles;
+let numCircles = 10
+let degrees = (2 * Math.PI) / numCircles
+let circleRadius = 100
+
+
+let colorTheme = [];
+
+for (let i = 0; i < numCircles; i++){
+  let r = Math.floor(Math.random() * 256)
+  let g = Math.floor(Math.random() * 256)
+  let b = Math.floor(Math.random() * 256)
+  colorTheme.push(`rgba(${r},${g},${b}, .25 )`)
+}
 
 function start() {
   canvas = document.getElementById('circle-canvas')
@@ -10,7 +21,7 @@ function start() {
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
   ctx.translate(canvas.width / 2, canvas.height / 2)
-  ctx.globalAlpha = .1
+  //ctx.globalAlpha = 0.1
   mainLoop()
 }
 
@@ -19,17 +30,20 @@ document.addEventListener('DOMContentLoaded', start)
 function update() {}
 
 function draw() {
-  ctx.fillStyle = 'blue'
+
   //ctx.fillRect(0, 0, canvas.width, canvas.height)
-  for (let i= 0 ; i <numCircles; i++) {
-    ctx.save();
-    ctx.beginPath();
-    ctx.ellipse(0,0, 50, 50, 0, 0, Math.PI*2)
+  for (let i = 0; i < numCircles; i++) {
+
+    ctx.fillStyle = colorTheme[i]
+    ctx.save()
+    ctx.beginPath()
+    ctx.translate(circleRadius, circleRadius*2)
+    ctx.ellipse(0, 0, circleRadius, circleRadius, 0, 0, Math.PI * 2)
     ctx.stroke()
     ctx.fill();
     ctx.restore()
+    ctx.rotate(degrees)
   }
-
 }
 function mainLoop() {
   update()
