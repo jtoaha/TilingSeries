ca80be95041918a3436bae7b8b929aa92207bb4a
Jamila Toaha <jtoahacoding@gmail.com>
2020-08-25 23:56:22 -0400

    (feat) added easing function for animation

--- CircleTiles/main.js
+++ CircleTiles/main.js
@@ -5,6 +5,7 @@ let numCircles = 10
 let degrees = (2 * Math.PI) / numCircles
 let circleRadius = 100
 
+let animX= 0;
 
 let colorTheme = [];
 
@@ -37,16 +38,25 @@ function draw() {
     ctx.fillStyle = colorTheme[i]
     ctx.save()
     ctx.beginPath()
-    ctx.translate(circleRadius, circleRadius*2)
+    ctx.translate(easeInQuad(animX), 0)
     ctx.ellipse(0, 0, circleRadius, circleRadius, 0, 0, Math.PI * 2)
     ctx.stroke()
     ctx.fill();
     ctx.restore()
     ctx.rotate(degrees)
   }
+  animX+=.1
 }
 function mainLoop() {
   update()
   draw()
   window.setTimeout(mainLoop, 1000 / 60)
+}
+
+function easeInSine(x){
+  return 1 - Math.cos((x * Math.PI) / 2);
+}
+function easeInQuad(x) {
+  //return 1 - Math.pow(1 - x, 3);
+  return x * x //ease in
 }
