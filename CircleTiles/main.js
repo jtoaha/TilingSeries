45ae9134d9429d7e5b2217244e9034fc127f4a6c
Jamila Toaha <jtoahacoding@gmail.com>
2020-08-28 23:58:31 -0400

    (feat) added light/dark theme and numCircles input

--- CircleTiles/main.js
+++ CircleTiles/main.js
@@ -5,11 +5,12 @@ let numCircles = 10
 let degrees = (2 * Math.PI) / numCircles
 let circleRadius = 100
 
-let animX= 0;
+let animX = 0
 
-let colorTheme = [];
+let theme = '#ffffff'
+let colorTheme = []
 
-for (let i = 0; i < numCircles; i++){
+for (let i = 0; i < numCircles; i++) {
   let r = Math.floor(Math.random() * 256)
   let g = Math.floor(Math.random() * 256)
   let b = Math.floor(Math.random() * 256)
@@ -31,7 +32,7 @@ document.addEventListener('DOMContentLoaded', start)
 function update() {}
 
 function draw() {
-  ctx.fillStyle = '#ffffff'
+  ctx.fillStyle = theme
   ctx.fillRect(
     -canvas.width / 2,
     -canvas.height / 2,
@@ -39,29 +40,56 @@ function draw() {
     canvas.height * 2
   )
   for (let i = 0; i < numCircles; i++) {
-
     ctx.fillStyle = colorTheme[i]
     ctx.save()
     ctx.beginPath()
     ctx.translate(easeInQuad(animX), 0)
     ctx.ellipse(0, 0, circleRadius, circleRadius, 0, 0, Math.PI * 2)
     ctx.stroke()
-    ctx.fill();
+    ctx.fill()
     ctx.restore()
     ctx.rotate(degrees)
   }
-  animX+=.1
+  animX += 0.1
 }
+
 function mainLoop() {
   update()
   draw()
   window.setTimeout(mainLoop, 1000 / 60)
 }
 
-function easeInSine(x){
-  return 1 - Math.cos((x * Math.PI) / 2);
+function easeInSine(x) {
+  return 1 - Math.cos((x * Math.PI) / 2)
 }
 function easeInQuad(x) {
   //return 1 - Math.pow(1 - x, 3);
   return x * x //ease in
 }
+
+//Add Event Handlers to input items
+$(document).ready(function () {
+  $('input[type=range]').click(function () {
+    //User can adjust number of rings
+    if (this.name === 'numCircles') {
+      console.log(this.value, 'numCircles') // future task: append to html
+      numCircles = this.value
+      //flag = 'reset'
+    }})
+  //User can choose dark theme or light theme
+  $('input[type=radio]').click(function () {
+    if (this.name === 'theme') {
+      if (this.value === 'light') {
+        theme = 'white'
+        flag = 'reset'
+      }
+      if (this.value === 'dark') {
+        console.log(this.value)
+        theme = 'black'
+        flag = 'reset'
+      }
+    }
+  })
+
+
+})
